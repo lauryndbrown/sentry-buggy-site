1ef62943ddb8fb834c174e2dec0ba85200431d1d
Lauryn Brown <lauryndbrown@gmail.com>
2018-11-29 10:57:20 -0800

    Added release to init sentry.

--- app.py
+++ app.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello, World!'
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def bug1(name=None):
+    return render_template('bug-1-hello-sentry.html', name=name)
+
+@app.route('/name-and-number/')
+def bug2():
+    return render_template('bug-2-name-and-number.html')
+
+@app.route('/count-down/')
+def bug3():
+    return render_template('bug-3-count-down.html')
+
