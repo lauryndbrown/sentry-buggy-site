83e8ee816f143acd6df5098ff57d0aea038d21e6
Lauryn Brown <lauryndbrown@gmail.com>
2018-12-06 15:46:57 -0800

    Made changes to hello bug

--- static/bug1.js
+++ static/bug1.js
@@ -1,12 +1,12 @@
-function say_hello(){
-    name = document.getElementById("name").value;
-    if(name.length < 1)
-        goodDayWorld = 'Hello Sentry! You got it right!!!';
-    else
-        helloWorld = `Hello ${name}!`;
-    showHello(helloWorld);
-}
-
 function showHello(message){
     document.getElementById("hello").innerHTML = message;
+}
+
+function sayHello(){
+    var name = document.getElementById("name").value;
+    if(name.length < 1)
+        var goodDayWorld = 'Hello Sentry! You got it right!!!';
+    else
+        var helloWorld = `Hello ${name}!`;
+    showHello(helloWorld);
 }
